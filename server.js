ddb8b79f8ec673bf6f04435fdc2bdbf52fdc0283
hoang van loi <stove654@gmail.com>
2014-11-12 22:38:23 +0700

    up code

--- server.js
+++ server.js
@@ -1,61 +1,48 @@
-// BASE SETUP
-// =============================================================================
 
-// call the packages we need
+
 var express    = require('express');
 var bodyParser = require('body-parser');
 var multiparty = require('multiparty');
 var app        = express();
 
-// configure app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port     = process.env.PORT || 8080; // set our port
+var port     = process.env.PORT || 8080;
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost/image'); // connect to our database
+mongoose.connect('mongodb://localhost/image');
 var Image     = require('./app/models/image');
 
-// ROUTES FOR OUR API
-// =============================================================================
 
-// create our router
 var router = express.Router();
 
-// middleware to use for all requests
 router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
     next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
-// on routes that end in /bears
-// ----------------------------------------------------
+
 router.route('/images')
 
-    // create a bear (accessed at POST http://localhost:8080/bears)
     .post(function(req, res) {
 
-        var image = new Image();		// create a new instance of the Bear model
-        image.name = req.body.name;  // set the bears name (comes from the request)
+        var image = new Image();
+        image.name = req.body.name;
 
-        image.save(function(err) {
+        image.save(function(err,data) {
             if (err)
                 res.send(err);
-
-            res.json({ message: 'Image created!' });
+            console.log(data);
+            res.json(data);
         });
 
 
     })
 
-    // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
         Image.find(function(err, images) {
             if (err)
@@ -65,8 +52,7 @@ router.route('/images')
         });
     });
 
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
+
 router.route('/images/:image_id')
 
     // get the bear with that id
@@ -96,7 +82,6 @@ router.route('/images/:image_id')
         });
     })
 
-    // delete the bear with this id
     .delete(function(req, res) {
         Image.remove({
             _id: req.params.image_id
@@ -109,10 +94,8 @@ router.route('/images/:image_id')
     });
 
 
-// REGISTER OUR ROUTES -------------------------------
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
+
 app.listen(port);
 console.log('Magic happens on port ' + port);
